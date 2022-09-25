0585e60cfe53c5d920d6bbae22f825f6d449aaaa
Anass <45261478+anasdeyra@users.noreply.github.com>
2022-09-25 14:23:13 +0100

    added vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon.png"],
+      manifest: {
+        name: "Todododo",
+        short_name: "Todo",
+        description: "A billion dollar todo app",
+        theme_color: "#6366F1",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
